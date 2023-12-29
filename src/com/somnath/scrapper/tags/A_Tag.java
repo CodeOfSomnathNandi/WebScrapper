f9c4e85a315e155afffd0b2eb38b08885ea544ba
Somnath Nandi <154261909+CodeOfSomnathNandi@users.noreply.github.com>
2023-12-29 18:21:06 +0530

    adding implementation for tags

--- src/com/somnath/scrapper/tags/A_Tag.java
+++ src/com/somnath/scrapper/tags/A_Tag.java
@@ -1,11 +1,14 @@
 package com.somnath.scrapper.tags;
 
+import com.somnath.scrapper.attributes.AAttributes;
 import com.somnath.scrapper.core.Element;
 
 public class A_Tag extends Element {
 
     public A_Tag(String content) {
         super("a", content);
+        AAttributes attributes = new AAttributes(content);
+        setAttributes(attributes);
     }
 
 }
